af66b7782c561d00cc0110e831f92282343a68f4
doGior <43815981+doGior@users.noreply.github.com>
2024-09-30 00:04:51 +0200

    Added item page in the StreamingCommunity plugin

--- StreamingCommunity/src/main/kotlin/it/dogior/doesStream/StreamingCommunity.kt
+++ StreamingCommunity/src/main/kotlin/it/dogior/doesStream/StreamingCommunity.kt
@@ -5,6 +5,7 @@ package it.dogior.doesStream
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.api.Log
 import com.lagradost.cloudstream3.APIHolder.capitalize
+import com.lagradost.cloudstream3.Episode
 import com.lagradost.cloudstream3.ErrorLoadingException
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.HomePageList
@@ -14,14 +15,18 @@ import com.lagradost.cloudstream3.LiveStreamLoadResponse
 import com.lagradost.cloudstream3.LoadResponse
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.MovieLoadResponse
 import com.lagradost.cloudstream3.MovieSearchResponse
 import com.lagradost.cloudstream3.SubtitleFile
+import com.lagradost.cloudstream3.TvSeriesLoadResponse
 import com.lagradost.cloudstream3.TvSeriesSearchResponse
 import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.app
 
 import com.lagradost.cloudstream3.mainPageOf
 import com.lagradost.cloudstream3.newHomePageResponse
+import com.lagradost.cloudstream3.newMovieLoadResponse
+import com.lagradost.cloudstream3.newTvSeriesLoadResponse
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
 
@@ -42,30 +47,30 @@ class StreamingCommunity : MainAPI() {
     }
 
     private val sectionNamesList = mainPageOf(
+        "$mainUrl/api/browse/top10" to "Top 10 di oggi",
         "$mainUrl/api/browse/trending" to "I Titoli Del Momento",
         "$mainUrl/api/browse/latest" to "Aggiunti di Recente",
-        "$mainUrl/api/browse/top10m" to "Top 10 di oggi",
-//        "$mainUrl/api/browse/top10t" to "Top 10 serie tv di oggi",
     )
 
     override val mainPage = sectionNamesList
 
     private fun searchResponseBuilder(
-        listJson: List<Title>,
-        filter: (Title) -> Boolean
+        listJson: List<Title>
     ): List<SearchResponse> {
-        val list: List<SearchResponse> = listJson.filter{ it.type == "movie" || it.type == "tv"}.filter(filter).map { title ->
+        val list: List<SearchResponse> = listJson.filter{ it.type == "movie" || it.type == "tv"}.map { title ->
+            val itemData = "@${title.name}§${title.score}·${title.slug}"
             if (title.type == "tv"){
                 TvSeriesSearchResponse(
                     name = title.name,
-                    url = "$mainUrl/titles/${title.id}-${title.slug}",
+                    url = "$mainUrl/api/titles/preview/${title.id}$itemData",
                     apiName = this@StreamingCommunity.name,
                     posterUrl = "https://cdn.streamingcommunity.computer/images/" + title.getPoster(),
+
                 )
             } else {
                 MovieSearchResponse(
                     name = title.name,
-                    url = "$mainUrl/titles/${title.id}-${title.slug}",
+                    url = "$mainUrl/api/titles/preview/${title.id}$itemData",
                     apiName = this@StreamingCommunity.name,
                     posterUrl = "https://cdn.streamingcommunity.computer/images/" + title.getPoster(),
                 )
@@ -90,15 +95,9 @@ class StreamingCommunity : MainAPI() {
                 Log.d(TAG, "LATEST")
             }
 
-            "top10m" -> {
-                url = request.data.substringBeforeLast("m")
-                params["type"] = "movie"
-                Log.d(TAG, "TOP10")
-            }
-
-            "top10t" -> {
-                url = request.data.substringBeforeLast("t")
-                params["type"] = "tv"
+            "top10" -> {
+//                url = request.data.substringBeforeLast("m")
+//                params["type"] = "movie"
                 Log.d(TAG, "TOP10")
             }
         }
@@ -112,7 +111,7 @@ class StreamingCommunity : MainAPI() {
         val responseJson = parseJson<Sezione>(responseString)
         Log.d(TAG, "Response: $responseJson")
 
-        val titlesList = searchResponseBuilder(responseJson.titles, filter = { true })
+        val titlesList = searchResponseBuilder(responseJson.titles)
 
         val hasNextPage =
             response.okhttpResponse.request.url.queryParameter("offset")?.toIntOrNull()
@@ -151,11 +150,53 @@ class StreamingCommunity : MainAPI() {
     }
 
     // This function gets called when you enter the page/show
-    override suspend fun load(url: String): LoadResponse? {
+    override suspend fun load(url: String): LoadResponse {
         val TAG = "STREAMINGCOMMUNITY:Item"
+//        Log.d(TAG, "Url: $url")
+        val name = url.substringAfterLast('@').substringBeforeLast('§')
+        val imdbScore = url.substringAfterLast('§').substringBeforeLast('·')
+        val slug = url.substringAfterLast('·')
+        val previewUrl = url.substringBeforeLast('·')
+        Log.d(TAG, "Name: $name")
+        Log.d(TAG, "IMDB: $imdbScore")
+        Log.d(TAG, "SLUG: $slug")
+        Log.d(TAG, "Url: $previewUrl")
+        val title = parseJson<TitlePreview>(app.post(previewUrl).body.string())
 
+        if (title.type == "tv") {
+            val episodes: List<Episode> = getEpisodes(title.id, slug)
 
-        return null
+            val tvShow = TvSeriesLoadResponse(
+                name = name,
+                url = previewUrl,
+                type = TvType.TvSeries,
+                apiName = this.name,
+                plot = title.plot,
+                posterUrl = "https://cdn.streamingcommunity.computer/images/" + title.getBackgroundImage(),
+                tags = listOf("IMDB: $imdbScore"),
+                episodes = episodes
+            )
+            Log.d(TAG, "TV Show: $tvShow")
+            return tvShow
+        } else {
+            val movie = MovieLoadResponse(
+                name = name,
+                url = previewUrl,
+                dataUrl = previewUrl,
+                type = TvType.Movie,
+                apiName = this.name,
+                plot = title.plot,
+                posterUrl = "https://cdn.streamingcommunity.computer/images/" + title.getBackgroundImage(),
+                tags = listOf("IMDB: $imdbScore"),
+            )
+
+            Log.d(TAG, "Movie: $movie")
+            return movie
+        }
+    }
+
+    private suspend fun getEpisodes(id: Int, slug: String): List<Episode> {
+        return emptyList()
     }
 
     // This function is how you load the links
@@ -186,8 +227,8 @@ class StreamingCommunity : MainAPI() {
         @JsonProperty("last_air_date") val last_air_date: String,
         @JsonProperty("age") val age: Int?, // Can be null or another type, modify if more info is available
         @JsonProperty("seasons_count") val seasons_count: Int,
-        @JsonProperty("images") val images: List<Poster>
-    ) {
+        @JsonProperty("images") val images: List<PosterImage>
+    ){
         fun getPoster(): String? {
             this.images.forEach {
                 if (it.type == "poster") {
@@ -198,13 +239,48 @@ class StreamingCommunity : MainAPI() {
         }
     }
 
-    data class Poster(
-        @JsonProperty("imageable_id") val imageable_id: Int,
-        @JsonProperty("imageable_type") val imageable_type: String,
+
+    data class PosterImage(
         @JsonProperty("filename") val filename: String,
         @JsonProperty("type") val type: String,
-        @JsonProperty("original_url_field") val original_url_field: String?
+        @JsonProperty("imageable_type") val imageable_type: String,
+        @JsonProperty("imageable_id") val imageable_id: Int,
     )
 
+    data class Pivot(
+        @JsonProperty("title_id") val title_id: Int,
+        @JsonProperty("genre_id") val genre_id: Int
+    )
 
+    data class Genre(
+        @JsonProperty("id") val id: Int,
+        @JsonProperty("name") val name: String,
+        @JsonProperty("type") val type: String,
+        @JsonProperty("hidden") val hidden: Int,
+        @JsonProperty("created_at") val created_at: String,
+        @JsonProperty("updated_at") val updated_at: String,
+        @JsonProperty("pivot") val pivot: Pivot
+    )
+
+    data class TitlePreview(
+        @JsonProperty("id") val id: Int,
+        @JsonProperty("type") val type: String,
+        @JsonProperty("runtime") val runtime: Int,
+        @JsonProperty("release_date") val release_date: String,
+        @JsonProperty("quality") val quality: String,
+        @JsonProperty("plot") val plot: String,
+        @JsonProperty("seasons_count") val seasons_count: Int,
+        @JsonProperty("genres") val genres: List<Genre>,
+        @JsonProperty("images") val images: List<PosterImage>
+    )
+    {
+        fun getBackgroundImage(): String? {
+            this.images.forEach {
+                if (it.type == "cover") {
+                    return it.filename
+                }
+            }
+            return null
+        }
+    }
 }
