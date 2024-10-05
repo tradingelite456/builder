baad4d9c86e83fe380884fc0214357d983531863
doGior <43815981+doGior@users.noreply.github.com>
2024-10-05 18:13:22 +0200

    New Homepage

--- AnimeWorld/src/main/kotlin/com/lagradost/AnimeWorld.kt
+++ AnimeWorld/src/main/kotlin/com/lagradost/AnimeWorld.kt
@@ -1,6 +1,7 @@
 package com.lagradost
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.api.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addAniListId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addDuration
@@ -28,15 +29,14 @@ class AnimeWorld : MainAPI() {
 
     companion object {
         private var cookies = emptyMap<String, String>()
-        private lateinit var token : String
+        private lateinit var token: String
+
         // Disabled authentication as site did
         private suspend fun request(url: String): NiceResponse {
 //            if (cookies.isEmpty()) {
 //                cookies = getCookies(url)
 //            }
-            return app.get(url
-//                , cookies = cookies
-            )
+            return app.get(url)
         }
 
         fun getType(t: String?): TvType {
@@ -56,14 +56,54 @@ class AnimeWorld : MainAPI() {
         }
     }
 
-    private fun Element.toSearchResult(showEpisode: Boolean = true): AnimeSearchResponse {
+    override val mainPage = mainPageOf(
+        "$mainUrl/tops/dubbed?sort=1" to "Top 100 Anime",
+        "$mainUrl/filter?status=0&language=it&sort=1" to "In Corso",
+        "$mainUrl/filter?language=it&sort=6" to "Più Visti",
+        "$mainUrl/filter?language=it&sort=1" to "Ultimi aggiunti",
+
+        // I wanted to include subbed anime, but tbh I don't really care.
+        // If you want them just uncomment these lines:
+
+        //"$mainUrl/filter?status=0&language=jp&sort=1" to "In Corso subbed",
+        //"$mainUrl/filter?language=jp&sort=6" to "Più Visti subbed",
+        //"$mainUrl/filter?language=jp&sort=1" to "Ultimi aggiunti subbed",
+    )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        Log.d("AnimeWorld:MainPage", "Url:${request.data}")
+        val pagedata = app.get(request.data)
+        val document = pagedata.document
+        val list = ArrayList<SearchResponse>()
+
+        if (request.name == "Top 100 Anime") {
+            //TODO
+            val items = document.select("div.row  .content")
+            Log.d("AnimeWorld:MainPage", "Items: ${items[0]}")
+            items.map { list.add(it.contentToSearchResult()) }
+        }
+        val items = document.select("div.film-list > .item")
+//        Log.d("AnimeWorld:MainPage", "Items: ${items[0]}")
+        items.map { list.add(it.toSearchResult()) }
+
+
+        return newHomePageResponse(
+            HomePageList(
+                name = request.name,
+                list = list,
+                isHorizontalImages = false
+            ), false
+        )
+    }
+
+    private fun Element.toSearchResult(): AnimeSearchResponse {
         fun String.parseHref(): String {
             val h = this.split('.').toMutableList()
             h[1] = h[1].substringBeforeLast('/')
             return h.joinToString(".")
         }
 
-        val anchor = this.select("a.name").firstOrNull() ?: throw ErrorLoadingException("Error")
+        val anchor = this.select("a.name").firstOrNull() ?: throw ErrorLoadingException("Error parsing the page")
         val title = anchor.text().removeSuffix(" (ITA)")
         val otherTitle = anchor.attr("data-jtitle").removeSuffix(" (ITA)")
 
@@ -73,8 +113,7 @@ class AnimeWorld : MainAPI() {
         val statusElement = this.select("div.status") // .first()
         val dub = statusElement.select(".dub").isNotEmpty()
 
-        val episode = if (showEpisode) statusElement.select(".ep").text().split(' ').last()
-            .toIntOrNull() else null
+
         val type = when {
             statusElement.select(".movie").isNotEmpty() -> TvType.AnimeMovie
             statusElement.select(".ova").isNotEmpty() -> TvType.OVA
@@ -82,42 +121,85 @@ class AnimeWorld : MainAPI() {
         }
 
         return newAnimeSearchResponse(title, url, type) {
-            addDubStatus(dub, episode)
+            addDubStatus(dub)
             this.otherName = otherTitle
             this.posterUrl = poster
         }
     }
 
-    override suspend fun getMainPage(page: Int, request : MainPageRequest): HomePageResponse {
-        val pagedata = request(mainUrl)
-        val document = pagedata.document
-        val list = ArrayList<HomePageList>()
-        token = document.getElementById("csrf-token")?.attr("content")?:""
-        cookies = pagedata.cookies
-        
-        val widget = document.select(".widget.hotnew")
-        widget.select(".tabs [data-name=\"sub\"], .tabs [data-name=\"dub\"]").forEach { tab ->
-            val tabId = tab.attr("data-name")
-            val tabName = tab.text().removeSuffix("-ITA")
-            val animeList = widget.select("[data-name=\"$tabId\"] .film-list .item").map {
-                it.toSearchResult()
-            }
-            list.add(HomePageList(tabName, animeList))
+    private fun Element.contentToSearchResult(): AnimeSearchResponse {
+        fun String.parseHref(): String {
+            val h = this.split('.').toMutableList()
+            h[1] = h[1].substringBeforeLast('/')
+            return h.joinToString(".")
         }
-        widget.select(".tabs [data-name=\"trending\"]").forEach { tab ->
-            val tabId = tab.attr("data-name")
-            val tabName = tab.text()
-            val animeList = widget.select("[data-name=\"$tabId\"] .film-list .item").map {
-                it.toSearchResult(showEpisode = false)
-            }.distinctBy { it.url }
-            list.add(HomePageList(tabName, animeList))
+
+        val anchor = this.selectFirst("a") ?: throw ErrorLoadingException("Error parsing the page")
+        val url = fixUrl(anchor.attr("href").parseHref())
+        val poster = anchor.select("img").attr("src")
+
+        val info = this.select("div.info > .main")
+
+        val name = info.select("a > .name").firstOrNull() ?: throw ErrorLoadingException("Error parsing the page")
+        val title = name.text().removeSuffix(" (ITA)")
+        val otherTitle = name.attr("data-jtitle").removeSuffix(" (ITA)")
+
+
+        val typeElement = this.select("div.type") // .first()
+        val dub = typeElement.select(".dub").isNotEmpty()
+
+
+        val type = when {
+            typeElement.select(".movie").isNotEmpty() -> TvType.AnimeMovie
+            typeElement.select(".ova").isNotEmpty() -> TvType.OVA
+            else -> TvType.Anime
         }
-        return HomePageResponse(list)
+
+        return newAnimeSearchResponse(title, url, type) {
+            addDubStatus(dub)
+            this.otherName = otherTitle
+            this.posterUrl = poster
+        }
     }
+
+    private fun Element.filterItemToSearchResult(): AnimeSearchResponse {
+        fun String.parseHref(): String {
+            val h = this.split('.').toMutableList()
+            h[1] = h[1].substringBeforeLast('/')
+            return h.joinToString(".")
+        }
+
+        val inner = this.select("div.inner")
+
+        val anchor = inner.select("a.name").first() ?: throw ErrorLoadingException("Error parsing the page")
+        val title = anchor.text().removeSuffix(" (ITA)")
+        val otherTitle = anchor.attr("data-jtitle").removeSuffix(" (ITA)")
+
+        val url = fixUrl(anchor.attr("href").parseHref())
+        val poster = this.select("a.poster img").attr("src")
+
+        val statusElement = this.select("div.status") // .first()
+        val dub = statusElement.select(".dub").isNotEmpty()
+
+
+        val type = when {
+            statusElement.select(".movie").isNotEmpty() -> TvType.AnimeMovie
+            statusElement.select(".ova").isNotEmpty() -> TvType.OVA
+            else -> TvType.Anime
+        }
+
+        return newAnimeSearchResponse(title, url, type) {
+            addDubStatus(dub)
+            this.otherName = otherTitle
+            this.posterUrl = poster
+        }
+    }
+
 
     data class searchJson(
         @JsonProperty("animes") val animes: List<animejson>
     )
+
     data class animejson(
         @JsonProperty("name") val name: String,
         @JsonProperty("image") val image: String,
@@ -129,9 +211,14 @@ class AnimeWorld : MainAPI() {
     )
 
     override suspend fun quickSearch(query: String): List<SearchResponse>? {
-        val document = app.post("https://www.animeworld.tv/api/search/v2?keyword=${query}", referer = mainUrl, cookies = cookies, headers = mapOf("csrf-token" to token)).text
+        val document = app.post(
+            "https://www.animeworld.tv/api/search/v2?keyword=${query}",
+            referer = mainUrl,
+            cookies = cookies,
+            headers = mapOf("csrf-token" to token)
+        ).text
 
-        return tryParseJson<searchJson>(document)?.animes?.map { anime->
+        return tryParseJson<searchJson>(document)?.animes?.map { anime ->
             val type = when (anime.type) {
                 "Movie" -> TvType.AnimeMovie
                 "OVA" -> TvType.OVA
@@ -148,10 +235,11 @@ class AnimeWorld : MainAPI() {
             }
         }
     }
+
     override suspend fun search(query: String): List<SearchResponse> {
         val document = request("$mainUrl/search?keyword=$query").document
         return document.select(".film-list > .item").map {
-            it.toSearchResult(showEpisode = false)
+            it.toSearchResult()
         }
     }
 
@@ -204,7 +292,7 @@ class AnimeWorld : MainAPI() {
         val comingSoon = episodes.isEmpty()
 
         val recommendations = document.select(".film-list.interesting .item").map {
-            it.toSearchResult(showEpisode = false)
+            it.toSearchResult()
         }
 
         return newAnimeLoadResponse(title, url, type) {
