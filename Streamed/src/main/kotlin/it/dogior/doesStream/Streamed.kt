82b420cd963399bba6f7b032f055026967d57186
doGior <43815981+doGior@users.noreply.github.com>
2024-11-02 15:18:32 +0100

    Minor change

--- Streamed/src/main/kotlin/it/dogior/doesStream/Streamed.kt
+++ Streamed/src/main/kotlin/it/dogior/doesStream/Streamed.kt
@@ -36,7 +36,7 @@ class Streamed : MainAPI() {
     override var mainUrl = Companion.mainUrl
     override var name = Companion.name
     override var supportedTypes = setOf(TvType.Live)
-    override var lang = "it"
+    override var lang = "en"
     override val hasMainPage = true
     override val hasDownloadSupport = false
     private val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
