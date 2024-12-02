2ebbef89b989bfb7a82c01df1cd59f541a4da6c7
doGior <43815981+doGior@users.noreply.github.com>
2024-12-02 20:48:06 +0100

    Added desugaring

--- YouTube/build.gradle.kts
+++ YouTube/build.gradle.kts
@@ -24,6 +24,18 @@ cloudstream {
     iconUrl = "https://www.youtube.com/s/desktop/711fd789/img/logos/favicon_144x144.png"
 }
 
+android{
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.3")
     implementation("com.github.teamnewpipe:NewPipeExtractor:v0.24.3")
 }
