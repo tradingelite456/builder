e5ff6dc3f4b0f7dafe5591d7deedd62ed202ecbe
doGior <43815981+doGior@users.noreply.github.com>
2025-05-13 01:05:27 +0200

    Updated ExampleProvider

--- ExampleProvider/src/main/kotlin/it/dogior/hadEnough/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/it/dogior/hadEnough/ExampleProvider.kt
@@ -1,41 +1,19 @@
 package it.dogior.hadEnough
 
 import android.util.Log
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.cloudstream3.Episode
-import com.lagradost.cloudstream3.HomePageList
 import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.LoadResponse
 import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.MainPageRequest
-import com.lagradost.cloudstream3.SearchQuality
 import com.lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.addPoster
-import com.lagradost.cloudstream3.amap
-import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.mainPageOf
-import com.lagradost.cloudstream3.newHomePageResponse
-import com.lagradost.cloudstream3.newMovieLoadResponse
-import com.lagradost.cloudstream3.newMovieSearchResponse
-import com.lagradost.cloudstream3.newTvSeriesLoadResponse
-import com.lagradost.cloudstream3.newTvSeriesSearchResponse
-import com.lagradost.cloudstream3.utils.AppUtils.parseJson
-import com.lagradost.cloudstream3.utils.AppUtils.toJson
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import com.lagradost.cloudstream3.utils.ExtractorLink
-import com.lagradost.cloudstream3.utils.loadExtractor
-import it.dogior.hadEnough.extractors.MaxStreamExtractor
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
-import okio.Buffer
-import org.json.JSONException
-import org.json.JSONObject
-import org.jsoup.Jsoup
+import com.lagradost.cloudstream3.app
 import org.jsoup.nodes.Document
+
 
 class ExampleProvider : MainAPI() {
     override var mainUrl = ""
