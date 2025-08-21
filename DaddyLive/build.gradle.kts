f4805c2e3495098bb3c671229ec78f0310e4ebb9
Trading elite <tradingviewelite2@gmail.com>
2025-08-21 16:21:01 +0200

    Update build.gradle.kts

--- DaddyLive/build.gradle.kts
+++ DaddyLive/build.gradle.kts
@@ -1,12 +1,23 @@
-// use an integer for version numbers
-version = 11
+import org.jetbrains.kotlin.konan.properties.Properties
 
+// use an integer for version numbers
+version = 6
+
+
+android {
+    defaultConfig {
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        android.buildFeatures.buildConfig=true
+        buildConfigField("String", "DaddyLive", "\"${properties.getProperty("DaddyLive")}\"")
+    }
+}
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Live TV from DaddyLive. You might need a VPN"
-    authors = listOf("doGior")
+    description = "Indian TV"
+    authors = listOf("Phisher98")
 
     /**
     * Status int as the following:
@@ -15,14 +26,13 @@ cloudstream {
     * 2: Slow
     * 3: Beta only
     * */
-    status = 1
+    status = 1 // will be 3 if unspecified
 
+    // List of video source types. Users are able to filter for extensions in a given category.
+    // You can find a list of available types here:
+    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
     tvTypes = listOf("Live")
+    iconUrl = "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/Icons/daddylive.png"
 
-    requiresResources = false
-
-    iconUrl = "https://raw.githubusercontent.com/doGior/doGiorsHadEnough/refs/heads/master/DaddyLive/live-streaming.png"
+    isCrossPlatform = false
 }
-//dependencies{
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
-//}
